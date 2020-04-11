396a87b3704e4ac863ea3ec2c901251ecf09377e
Mohsin Ali <engr.syedmohsinalizaidi@gmail.com>
2020-04-12 00:40:19 +0500

    settings complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroSliderComponent } from './hero-slider/hero-slider.component';
+import { FeatureComponent } from './feature/feature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroSliderComponent,
+    FeatureComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-slider/hero-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-slider',
+  templateUrl: './hero-slider.component.html',
+  styleUrls: ['./hero-slider.component.css']
+})
+export class HeroSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
