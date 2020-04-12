8cc142973175a6e79458ceee4ecfd300a2db4d8f
Mohsin Ali <engr.syedmohsinalizaidi@gmail.com>
2020-04-12 20:27:30 +0500

    fix change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroSliderComponent } from './hero-slider/hero-slider.component';
 import { FeatureComponent } from './feature/feature.component';
+import { ProcessComponent } from './process/process.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeroSliderComponent,
-    FeatureComponent
+    FeatureComponent,
+    ProcessComponent
   ],
   imports: [
     BrowserModule,
